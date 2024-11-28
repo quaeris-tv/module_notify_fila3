ab635bbfd56d6dad782471a8819cb6d3c38218e6
Davide Tricella <dadi.tricella@gmail.com>
2024-11-28 15:31:35 +0100

    up

--- Resources/lang/it/send_email.php
+++ Resources/lang/it/send_email.php
@@ -1,39 +1,48 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'resource' => [
-        'name' => 'send_email',
-    ],
-    'navigation' => [
-        'name' => 'send_email',
-        'plural' => 'send_email',
-        'group' => [
-            'name' => 'Invia',
-        ],
-    ],
-    'fields' => [
-        'name' => 'Nome Area',
-        'parent' => 'Settore di appartenenza',
-        'parent.name' => 'Settore di appartenenza',
-        'parent_name' => 'Settore di appartenenza',
-        'assets' => 'Quantità di asset',
-    ],
-    'actions' => [
-        'import' => [
-            'name' => 'Importa da file',
-            'fields' => [
-                'import_file' => 'Seleziona un file XLS o CSV da caricare',
-            ],
-        ],
-        'export' => [
-            'name' => 'Esporta dati',
-            'filename_prefix' => 'Aree al',
-            'columns' => [
-                'name' => 'Nome area',
-                'parent_name' => 'Nome area livello superiore',
-            ],
-        ],
-    ],
-];
+<?php return array (
+  'resource' => 
+  array (
+    'name' => 'send_email',
+  ),
+  'navigation' => 
+  array (
+    'name' => 'send_email',
+    'plural' => 'send_email',
+    'group' => 
+    array (
+      'name' => 'Invia',
+    ),
+  ),
+  'fields' => 
+  array (
+    'name' => 'Nome Area',
+    'parent' => 'Settore di appartenenza',
+    'parent.name' => 'Settore di appartenenza',
+    'parent_name' => 'Settore di appartenenza',
+    'assets' => 'Quantità di asset',
+    'body_html' => 
+    array (
+      'label' => 'body_html',
+    ),
+  ),
+  'actions' => 
+  array (
+    'import' => 
+    array (
+      'name' => 'Importa da file',
+      'fields' => 
+      array (
+        'import_file' => 'Seleziona un file XLS o CSV da caricare',
+      ),
+    ),
+    'export' => 
+    array (
+      'name' => 'Esporta dati',
+      'filename_prefix' => 'Aree al',
+      'columns' => 
+      array (
+        'name' => 'Nome area',
+        'parent_name' => 'Nome area livello superiore',
+      ),
+    ),
+  ),
+);
