4436faa7c6eaf2d5453e4c1891608eb55e9f2803
marco76tv <marco.sottana@gmail.com>
2025-01-14 13:21:53 +0100

    up

--- lang/it/notify_theme.php
+++ lang/it/notify_theme.php
@@ -1,52 +1,36 @@
 <?php 
-return array (
-  'resource' => 
-  array (
-    'name' => 'notify_themes',
-  ),
-  'navigation' => 
-  array (
+return [
+  'resource' => ['name' => 'notify_themes'],
+  'navigation' => [
     'name' => 'notify_themes',
     'plural' => 'notify_themes',
-    'group' => 
-    array (
-      'name' => 'Admin',
-    ),'
+    'group' => ['name' => 'Admin'],
     'label' => 'notify theme',
-  ),
-  'fields' => 
-  array (
+  ],
+  'fields' => [
     'name' => 'Nome Area',
     'parent' => 'Settore di appartenenza',
     'parent.name' => 'Settore di appartenenza',
     'parent_name' => 'Settore di appartenenza',
     'assets' => 'Quantità di asset',
-  ),
-  'actions' => 
-  array (
-    'import' => 
-    array (
+  ],
+  'actions' => [
+    'import' => [
       'name' => 'Importa da file',
-      'fields' => 
-      array (
-        'import_file' => 'Seleziona un file XLS o CSV da caricare',
-      ),
-    ),
-    'export' => 
-    array (
+      'fields' => ['import_file' => 'Seleziona un file XLS o CSV da caricare'],
+    ],
+    'export' => [
       'name' => 'Esporta dati',
       'filename_prefix' => 'Aree al',
-      'columns' => 
-      array (
+      'columns' => [
         'name' => 'Nome area',
         'parent_name' => 'Nome area livello superiore',
-      ),
-    ),
-  ),
-  'tab' => 
-  array (
+      ],
+    ],
+  ],
+  'tab' => [
     'index' => 'Lista',
     'create' => 'Aggiungi',
     'edit' => 'Modifica',
-  ),
-);
+  ],
+];
