0a03e013ca41af24a370613e388f9bccd9449ec1
marco76tv <marco.sottana@gmail.com>
2025-01-14 13:42:35 +0100

    up

--- lang/it/notify_theme.php
+++ lang/it/notify_theme.php
@@ -1,43 +1,41 @@
-<?php 
+<?php
+
+declare(strict_types=1);
+
 return [
-  'resource' => ['name' => 'notify_themes'],
-  'navigation' => [
-    'name' => 'notify_themes',
-    'plural' => 'notify_themes',
-<<<<<<< HEAD
-    'group' => ['name' => 'Admin'],
-=======
-    'group' => 
-    array (
-      'name' => 'Admin',
-    ),
->>>>>>> origin/dev
-    'label' => 'notify theme',
-  ],
-  'fields' => [
-    'name' => 'Nome Area',
-    'parent' => 'Settore di appartenenza',
-    'parent.name' => 'Settore di appartenenza',
-    'parent_name' => 'Settore di appartenenza',
-    'assets' => 'Quantità di asset',
-  ],
-  'actions' => [
-    'import' => [
-      'name' => 'Importa da file',
-      'fields' => ['import_file' => 'Seleziona un file XLS o CSV da caricare'],
+    'resource' => ['name' => 'notify_themes'],
+    'navigation' => [
+        'name' => 'notify_themes',
+        'plural' => 'notify_themes',
+        'group' => [
+            'name' => 'Admin',
+        ],
+        'label' => 'notify theme',
     ],
-    'export' => [
-      'name' => 'Esporta dati',
-      'filename_prefix' => 'Aree al',
-      'columns' => [
-        'name' => 'Nome area',
-        'parent_name' => 'Nome area livello superiore',
-      ],
+    'fields' => [
+        'name' => 'Nome Area',
+        'parent' => 'Settore di appartenenza',
+        'parent.name' => 'Settore di appartenenza',
+        'parent_name' => 'Settore di appartenenza',
+        'assets' => 'Quantità di asset',
     ],
-  ],
-  'tab' => [
-    'index' => 'Lista',
-    'create' => 'Aggiungi',
-    'edit' => 'Modifica',
-  ],
+    'actions' => [
+        'import' => [
+            'name' => 'Importa da file',
+            'fields' => ['import_file' => 'Seleziona un file XLS o CSV da caricare'],
+        ],
+        'export' => [
+            'name' => 'Esporta dati',
+            'filename_prefix' => 'Aree al',
+            'columns' => [
+                'name' => 'Nome area',
+                'parent_name' => 'Nome area livello superiore',
+            ],
+        ],
+    ],
+    'tab' => [
+        'index' => 'Lista',
+        'create' => 'Aggiungi',
+        'edit' => 'Modifica',
+    ],
 ];
