12835ceb364db7cf28dced5dcd50dae6ff1fda77
marco76tv <marco.sottana@gmail.com>
2025-01-20 22:36:39 +0100

    up

--- app/Filament/Resources/NotificationResource/Pages/ViewNotification.php
+++ app/Filament/Resources/NotificationResource/Pages/ViewNotification.php
@@ -4,16 +4,15 @@ namespace Modules\Notify\Filament\Resources\NotificationResource\Pages;
 
 use Filament\Actions;
 use Modules\Notify\Filament\Resources\NotificationResource;
-use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
 
-class ListNotifications extends XotBaseListRecords
+class ViewNotification extends XotBaseViewRecord
 {
     protected static string $resource = NotificationResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\ViewAction::make(),
             Actions\EditAction::make(),
             Actions\DeleteAction::make()
                 ->action(fn () => $this->record->softDelete()),
