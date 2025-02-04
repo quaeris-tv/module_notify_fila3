4f1d1c875789054c33684a4cb97e21d6aa7aeeeb
marco76tv <marco.sottana@gmail.com>
2025-02-04 12:58:21 +0100

    up

--- app/Filament/Resources/NotifyThemeResource/Pages/ListNotifyThemes.php
+++ app/Filament/Resources/NotifyThemeResource/Pages/ListNotifyThemes.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Filament\Resources\NotifyThemeResource\Pages;
 
-<<<<<<< HEAD
 use Filament\Actions;
 use Filament\Tables\Actions as TableActions;
 use Filament\Tables\Columns;
 use Filament\Tables\Filters;
 use Filament\Resources\Pages\ListRecords;
-=======
-use Filament\Actions\CreateAction;
-use Filament\Resources\Pages\ListRecords\Tab;
-use Filament\Tables\Actions\EditAction;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\ViewAction;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Filters\SelectFilter;
-use Modules\Notify\Filament\Resources\NotifyThemeResource;
-use Modules\Notify\Models\NotifyTheme;
->>>>>>> origin/dev
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 use Modules\Notify\Filament\Resources\NotifyThemeResource;
 
@@ -31,7 +19,6 @@ class ListNotifyThemes extends XotBaseListRecords
     public function getListTableColumns(): array
     {
         return [
-<<<<<<< HEAD
             'id' => Columns\TextColumn::make('id')
                 ->sortable(),
             'lang' => Columns\TextColumn::make('lang')
@@ -54,80 +41,6 @@ class ListNotifyThemes extends XotBaseListRecords
                 ->dateTime()
                 ->sortable()
                 ->toggleable(isToggledHiddenByDefault: true)
-=======
-            TextColumn::make('id')
-                ->sortable()
-                ->searchable(),
-            TextColumn::make('lang')
-                ->sortable()
-                ->searchable(),
-            TextColumn::make('type')
-                ->sortable()
-                ->searchable(),
-            TextColumn::make('subject')
-                ->sortable()
-                ->searchable()
-                ->limit(50),
-            TextColumn::make('theme')
-                ->sortable()
-                ->searchable(),
-            TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable(),
-        ];
-    }
-
-    public function getTableFilters(): array
-    {
-        return [
-            SelectFilter::make('lang')
-                ->options(NotifyThemeResource::fieldOptions('lang')),
-            SelectFilter::make('type')
-                ->options(NotifyThemeResource::fieldOptions('type')),
-            SelectFilter::make('theme')
-                ->options([
-                    'empty' => 'empty',
-                    'ark' => 'ark',
-                    'minty' => 'minty',
-                    'sunny' => 'sunny',
-                    'widgets' => 'widgets',
-                ]),
-        ];
-    }
-
-    public function getTableActions(): array
-    {
-        return [
-            ViewAction::make(),
-            EditAction::make(),
-        ];
-    }
-
-    public function getTableBulkActions(): array
-    {
-        return [
-            DeleteBulkAction::make(),
-        ];
-    }
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
-
-    public function getTabs(): array
-    {
-        return [
-            'all' => Tab::make('Tutti'),
-            'email' => Tab::make('Email')
-                ->modifyQueryUsing(fn ($query) => $query->where('type', 'email')),
-            'sms' => Tab::make('SMS')
-                ->modifyQueryUsing(fn ($query) => $query->where('type', 'sms')),
-            'telegram' => Tab::make('Telegram')
-                ->modifyQueryUsing(fn ($query) => $query->where('type', 'telegram')),
->>>>>>> origin/dev
         ];
     }
 
