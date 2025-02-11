e0148cfef9d053f7324923897c67c882dbb26cd1
marco76tv <marco.sottana@gmail.com>
2025-02-11 08:35:11 +0100

    up

---
+++ App/Filament/Resources/NotificationResource/Pages/ListNotifications.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Notify\Filament\Resources\NotificationResource\Pages;
+
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Modules\Notify\Filament\Resources\NotificationResource;
+use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+
+class ListNotifications extends XotBaseListRecords
+{
+    protected static string $resource = NotificationResource::class;
+
+    public function getListTableColumns(): array
+    {
+        return [
+            'id' => TextColumn::make('id')
+                ->numeric()
+                ->sortable(),
+            'type' => TextColumn::make('type')
+                ->searchable()
+                ->sortable(),
+            'notifiable' => TextColumn::make('notifiable.name')
+                ->searchable()
+                ->sortable(),
+            'data' => TextColumn::make('data')
+                ->searchable(),
+            'read_at' => TextColumn::make('read_at')
+                ->dateTime()
+                ->sortable(),
+            'created_at' => TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable(),
+            'updated_at' => TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable(),
+        ];
+    }
+
+    public function getTableFilters(): array
+    {
+        return [
+            \Filament\Tables\Filters\SelectFilter::make('type')
+                ->options(fn () => \Modules\Notify\Models\Notification::distinct()->pluck('type', 'type')->toArray()),
+            \Filament\Tables\Filters\Filter::make('read')
+                ->query(fn ($query) => $query->whereNotNull('read_at')),
+            \Filament\Tables\Filters\Filter::make('unread')
+                ->query(fn ($query) => $query->whereNull('read_at')),
+        ];
+    }
+
+    public function getTableActions(): array
+    {
+        return [
+            ViewAction::make(),
+            EditAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+
+    public function getTableBulkActions(): array
+    {
+        return [
+            \Filament\Tables\Actions\DeleteBulkAction::make(),
+        ];
+    }
+}
