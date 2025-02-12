01db8e715bbbf91960e47421b877309b3cf83d3c
marco76tv <marco.sottana@gmail.com>
2025-02-12 08:37:49 +0100

    up

---
+++ lang/it/notification.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'resource' => [
+        'name' => 'notification',
+    ],
+    'navigation' => [
+        'name' => 'notification',
+        'plural' => 'notifications',
+        'group' => [
+            'name' => 'Admin',
+        ],
+        'label' => 'notification',
+        'sort' => 84,
+        'icon' => 'notification.navigation',
+    ],
+    'fields' => [
+        'name' => 'Nome Area',
+        'parent' => 'Settore di appartenenza',
+        'parent.name' => 'Settore di appartenenza',
+        'parent_name' => 'Settore di appartenenza',
+        'assets' => 'Quantità di asset',
+        'type' => [
+            'name' => 'Tipo',
+        ],
+    ],
+    'actions' => [
+        'import' => [
+            'name' => 'Importa da file',
+            'fields' => [
+                'import_file' => 'Seleziona un file XLS o CSV da caricare',
+            ],
+        ],
+        'export' => [
+            'name' => 'Esporta dati',
+            'filename_prefix' => 'Aree al',
+            'columns' => [
+                'name' => 'Nome area',
+                'parent_name' => 'Nome area livello superiore',
+            ],
+        ],
+    ],
+];
