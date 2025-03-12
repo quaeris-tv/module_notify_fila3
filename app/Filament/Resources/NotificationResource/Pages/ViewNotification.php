b501f033244a652265c41dc53dfc1b959888d9cd
marco76tv <marco.sottana@gmail.com>
2025-03-12 12:55:34 +0100

    up

--- app/Filament/Resources/NotificationResource/Pages/ViewNotification.php
+++ app/Filament/Resources/NotificationResource/Pages/ViewNotification.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Notify\Filament\Resources\NotificationResource\Pages;
 
-use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
 use Modules\Notify\Filament\Resources\NotificationResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseViewRecord;
 
 class ViewNotification extends XotBaseViewRecord
 {
     protected static string $resource = NotificationResource::class;
-    
+
     /**
      * @return array<\Filament\Infolists\Components\Component>
      */
@@ -32,7 +32,7 @@ class ViewNotification extends XotBaseViewRecord
                         ->dateTime(),
                     'updated_at' => TextEntry::make('updated_at')
                         ->dateTime(),
-                ])
+                ]),
         ];
     }
 }
